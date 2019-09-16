334d6548f0fb323abb796512c9742505c6a74436
beezmo <37284613+beezmo@users.noreply.github.com>
2019-09-15 23:05:09 -0700

    Final visualization code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -25,6 +25,8 @@ var chartGroup = svg.append("g")
 // Import Data
 d3.csv("assets/data/data.csv").then(function(healthData) {
 
+    console.log(healthData);
+
     // Cast as numerals
     healthData.forEach(function(data) {
         data.obesity = +data.obesity;
@@ -33,11 +35,66 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
 
     // Scale Functions
     var xLinearScale = d3.scaleLinear()
-        .domain(d3.extent(healthData, d => d.poverty))
+        .domain([8.5, d3.max(healthData, d => d.poverty)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
         .domain([0, d3.max(healthData, d => d.obesity)])
         .range([height, 0]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append to chart
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    chartGroup.append("g")
+    .call(leftAxis);
+
+    // Build circles
+    var circlesGroup = chartGroup.selectAll("Circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "15")
+    .attr("fill", "lightblue")
+    .attr("opacity", "0.8");
+
+    // Build circle labels
+    var circleLabels = chartGroup.selectAll(null).data(healthData).enter().append("text");
+
+    circleLabels
+    .attr("x", function(d) {
+        return xLinearScale(d.poverty);
+    })
+    .attr("y", function(d) {
+        return yLinearScale(d.obesity);
+    })
+    .text(function(d) {
+        return d.abbr;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "10px")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white");
+
+    // Build axis labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Obesity (%)");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
 
 });
